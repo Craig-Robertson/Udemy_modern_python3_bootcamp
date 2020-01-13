48bfcb86278d2aaf56cfbbeff53d068778251779
Craig Robertson <craig.robertson6@gmail.com>
2020-01-13 07:26:17 -0500

    for and while loop exercises 1-13-20

---
+++ Clean your room exercise.py
@@ -0,0 +1,4 @@
+times = input("How many times do I have to tell you this? ")
+times = int(times)
+for time in range(times):
+	print(f"time {time+1}: Clean your room !")
---
+++ Intro while loops.py
@@ -0,0 +1,10 @@
+# msg = input("what is the password?")
+# while msg != "bananas":
+# 	print("WRONG!")
+# 	msg = input("what is the password?")
+# print("correct!")
+
+num = 1
+while num < 11:
+	print(num)
+	num += 2
